b2a569ac7458ff0df9b58252f389204f30e29013
Chenguang Shen <pscggeforce@gmail.com>
2013-09-30 18:08:23 -0700

    update

--- src/edu/ucla/ee/nesl/privacyfilter/filtermanager/AppDetailActivityWithMap.java
+++ src/edu/ucla/ee/nesl/privacyfilter/filtermanager/AppDetailActivityWithMap.java
@@ -150,7 +150,7 @@ public class AppDetailActivityWithMap extends FragmentActivity implements Action
         @Override
         public CharSequence getPageTitle(int position) {
             if (position == 0) {
-            	return "App Detail";
+            	return "Rule Config";
             }
             else if (position == 1) {
             	return "External Context";
